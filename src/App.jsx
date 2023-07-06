d8ca2ebf46c3d47b91f4eefc4445fbe60fd060cc
sundaramUTS <sundaram.n@uplers.in>
2023-07-06 12:53:16 +0530

    Add browser router

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "@Layout/layout";
 import store from "./redux_toolkit/store/store";
 import { Provider } from "react-redux";
 import ChatListing from "./components/chat-list/chatListing";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => (
   <div className="container">
@@ -14,7 +15,9 @@ const App = () => (
 );
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("app")
 );
